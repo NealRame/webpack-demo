108c5f322f3ee26eb8dcb52e19eb9fe45c6e3e5f
Julien Graziano <julien@graziano.fr>
2022-06-14 01:15:14 +0200

    refactor: load images assets

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import _ from "lodash"
 
 import "./style.css"
+import Icon from "./icon.png"
+
 function component() {
+    const myIcon = new Image()
+    myIcon.src = Icon
+
     const element = document.createElement('div')
+
     element.innerHTML = _.join(['Hello', 'webpack'], ' ')
     element.classList.add('hello')
+    element.appendChild(myIcon)
+
     return element
 }
 
