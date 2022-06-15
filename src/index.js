5288c75a365f6ef24e54a8a8d369da618d2fe9a6
Julien Graziano <julien@graziano.fr>
2022-06-15 15:49:32 +0200

    refactor: html-webpack-plugin

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import _ from "lodash"
 
+import printMe from "./print.js"
+
 function component() {
-    const element = document.createElement('div')
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+    const btn = document.createElement("button")
+    btn.innerHTML = "Click me and check the console!"
+    btn.onclick = printMe
+
+    const element = document.createElement("div")
+    element.innerHTML = _.join(["Hello", "webpack"], " ")
+    element.appendChild(btn)
+
     return element
 }
 
