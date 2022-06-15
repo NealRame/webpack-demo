8c93d6589b2e1a444c4f4ed6f7502ba57e3f0a24
Julien Graziano <julien@graziano.fr>
2022-06-16 01:15:17 +0200

    refactor: module identifiers

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import _ from "lodash"
 
+import print from "./print"
+
 function component() {
     const element = document.createElement("div")
     element.innerHTML = _.join(["Hello", "webpack"], " ")
+    element.onclick = print.bind(null, "Hello webpack")
     return element
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,7 @@ module.exports = {
         path: path.resolve(__dirname, "dist"),
     },
     optimization: {
+        moduleIds: "deterministic",
         runtimeChunk: "single",
         splitChunks: {
             cacheGroups: {
