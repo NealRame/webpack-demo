38b6c29899735c01ca96a607c77a2a98ca7c340b
Julien Graziano <julien@graziano.fr>
2022-06-15 15:09:45 +0200

    refactor: wrapping up

--- src/index.js
+++ src/index.js
@@ -1,30 +1,8 @@
 import _ from "lodash"
 
-import "./style.css"
-import Icon from "./icon.png"
-
-import books from "./books.json"
-import computers from "./computers.yml"
-import games from "./games.csv"
-import music from "./music.toml"
-import pets from "./pets.xml"
-
 function component() {
-    const myIcon = new Image()
-    myIcon.src = Icon
-
     const element = document.createElement('div')
-
     element.innerHTML = _.join(['Hello', 'webpack'], ' ')
-    element.classList.add('hello')
-    element.appendChild(myIcon)
-
-    console.log(books)
-    console.log(computers)
-    console.log(games)
-    console.log(music)
-    console.log(pets)
-
     return element
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,9 @@
 const path = require("path")
-const toml = require("toml")
-const yaml = require("yamljs")
-
 
 module.exports = {
     entry: "./src/index.js",
     output: {
         filename: "bundle.js",
         path: path.resolve(__dirname, "dist")
-    },
-    module: {
-        rules: [{
-            test: /\.css$/i,
-            use: ["style-loader", "css-loader"],
-        }, {
-            test: /\.xml$/i,
-            use: ["xml-loader"],
-        }, {
-            test: /\.csv$/i,
-            use: ["csv-loader"],
-        }, {
-            test: /\.toml$/i,
-            type: "json",
-            parser: {
-                parse: toml.parse
-            },
-        }, {
-            test: /\.(yml|yaml)$/i,
-            type: "json",
-            parser: {
-                parse: yaml.parse
-            },
-        }, {
-            test: /(png|svg|jpg|jpeg|gif)$/i,
-            type: "asset/resource",
-        }, {
-            test: /\.(woff|woff2|eot|ttf|otf)$/i,
-            type: "asset/resource",
-        }],
     },
 }
