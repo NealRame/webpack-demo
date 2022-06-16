2bec2e550de1a98fb3eb186a301b009faa399fd8
Julien Graziano <julien@graziano.fr>
2022-06-16 15:07:12 +0200

    refactor: externalize lodash

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,14 @@ const path = require("path")
 module.exports = {
     mode: "development",
     entry: "./src/index.js",
+    externals: {
+        lodash: {
+            commonjs: "lodash",
+            commonjs2: "lodash",
+            amd: "lodash",
+            root: "_",
+        },
+    },
     output: {
         clean: true,
         filename: "webpack-numbers.js",
